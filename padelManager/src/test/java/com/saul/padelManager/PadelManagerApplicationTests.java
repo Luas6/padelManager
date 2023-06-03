45062e2b9ad900af9ec4a489e37ca0ab1c0f1ed9
ASUSDESAUL\saulm <saulmaji@gmail.com>
2023-06-03 12:47:12 +0200

    Commit Inicial - Spring Start Seed

---
+++ padelManager/src/test/java/com/saul/padelManager/PadelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saul.padelManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
