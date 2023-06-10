778cfde5f4e6d62bda4f6cc2fa310748889b476d
Saul Matías Jiménez <saulmaji@gmail.com>
2023-06-10 20:16:32 +0200

    Post Crear Empleado

--- padelManager/src/main/java/com/saul/padelManager/gestionUsuarios/controller/EmpleadoController.java
+++ padelManager/src/main/java/com/saul/padelManager/gestionUsuarios/controller/EmpleadoController.java
@@ -1,12 +1,12 @@
 package com.saul.padelManager.gestionUsuarios.controller;
 
+import com.saul.padelManager.gestionUsuarios.exceptions.ResourceNotFoundException;
 import com.saul.padelManager.gestionUsuarios.model.Empleado;
 import com.saul.padelManager.gestionUsuarios.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +15,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/")
 public class EmpleadoController {
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    /* CRUD Empleados*/
+
+    // Sacar todos los empleados
+    @GetMapping("/empleados")
+    public List<Empleado> getAllEmpleados(){
+        return empleadoRepository.findAll();
+    }
+
+    // Recuperar empleado por id
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> getEmployeeById(@PathVariable Long id) {
+        Empleado employee = empleadoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    // Crear Empleado
+    @PostMapping("/empleados")
+    public Empleado createEmpleado(@RequestBody Empleado empleado) {
+        return empleadoRepository.save(empleado);
+    }
+
+    /* Solucionar Error CORS*/
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -26,14 +53,6 @@ public class EmpleadoController {
                         .allowedHeaders("*");
             }
         };
-    }
-
-    @Autowired
-    private EmpleadoRepository empleadoRepository;
-
-    @GetMapping("/empleados")
-    public List<Empleado> getAllEmpleados(){
-        return empleadoRepository.findAll();
     }
 
 }
