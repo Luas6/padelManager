9febe69bd23c23a31c32b0110c8e46e1c56db310
Saul Matías Jiménez <saulmaji@gmail.com>
2023-09-30 23:22:44 +0200

    Mensaje error formulario registro

--- padelManager-frontend/src/app/usuarios/crear-usuario/crear-usuario.component.ts
+++ padelManager-frontend/src/app/usuarios/crear-usuario/crear-usuario.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Usuario } from '../usuario';
 import { UsuarioService } from '../usuario.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-crear-usuario',
   templateUrl: './crear-usuario.component.html',
   styleUrls: ['./crear-usuario.component.css']
 })
+
 export class CrearUsuarioComponent implements OnInit{
+
   usuario: Usuario = new Usuario();
+  formError = false;
+  private modalRef: any;
+  @ViewChild('errorModal') errorModal: any;
+
   constructor(private usuarioService: UsuarioService,
+    private modalService: NgbModal,
     private router: Router) { }
 
   ngOnInit(): void {
@@ -25,14 +33,25 @@ export class CrearUsuarioComponent implements OnInit{
   }
 
   goToUsuarioList(){
-    this.router.navigate(['/usuarios']);
+    this.router.navigate(['/login-usuario']);
   }
   
+  open(content: any) {
+    this.modalRef = this.modalService.open(content);
+  }
+
+  close() {
+    this.modalRef.close();
+  }
+
   onSubmit(usuarioForm: NgForm) {
     if (usuarioForm.valid) {
         // El formulario es válido, realiza la acción
         console.log(this.usuario);
         this.saveUsuario();
+    }else{
+      this.formError = true;
+      this.open(this.errorModal);
     }
 }
 }
