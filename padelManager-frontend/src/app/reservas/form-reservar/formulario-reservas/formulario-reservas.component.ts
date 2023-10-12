ab188867868ce46837cf20d52b114398aeb1a916
Saul Matías Jiménez <saulmaji@gmail.com>
2023-10-13 00:01:42 +0200

    Formulario Reservas como FormGroup

--- padelManager-frontend/src/app/reservas/form-reservar/formulario-reservas/formulario-reservas.component.ts
+++ padelManager-frontend/src/app/reservas/form-reservar/formulario-reservas/formulario-reservas.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Reserva } from '../../reserva';
 import { ReservasService } from '../../reservas.service';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reservas',
@@ -12,19 +12,24 @@ import { NgForm } from '@angular/forms';
 export class FormularioReservasComponent {
   reserva: Reserva = new Reserva();
   listaDeReservasExistente: Reserva[] = [];
+  reservasForm: FormGroup;
   errorMensaje: string = '';
 
   constructor(private reservasService: ReservasService,
-    private router: Router) { }
+    private router: Router) {
+      this.reservasForm = new FormGroup({
+        hora: new FormControl('', [Validators.required]),
+        pista: new FormControl('', [Validators.required]),
+        fecha: new FormControl('', [Validators.required]),
+      });
+    }
 
   ngOnInit(): void {
     this.getReservasExistente();
   }
 
   saveReserva() {
-    this.reservasService.crearReserva(this.reserva).subscribe(data => {
-      console.log(data);
-      this.goToHome();
+    this.reservasService.crearReserva(this.reservasForm.value).subscribe(data => {
     },
       error => console.log(error));
   }
@@ -33,24 +38,14 @@ export class FormularioReservasComponent {
     this.router.navigate(['']);
   }
 
-  onSubmit(reservaForm: NgForm) {
-    if (reservaForm.valid) {
-      if (this.isFechaHoraDisponible(this.reserva.fecha, this.reserva.hora, this.reserva.pista, this.listaDeReservasExistente)) {
-        // La fecha y hora están disponibles, realiza la reserva
-        console.log(this.reserva);
-        this.saveReserva();
-      } else {
-        // La fecha y hora ya están reservadas, muestra un mensaje de error
-        this.errorMensaje = "La fecha y hora seleccionadas ya están reservadas.";
-        console.log("La fecha y hora seleccionadas ya están reservadas.");
-        // También puedes desactivar el botón de reserva o mostrar una alerta al usuario.
-      }
+  onSubmit() {
+    if (this.reservasForm.valid) {
+      this.saveReserva();
+      this.goToHome();
+    } else {
+      this.errorMensaje = "La fecha y hora seleccionadas ya están reservadas.";
     }
-  }
 
-  isFechaHoraDisponible(fecha: string | undefined, hora: string | undefined, pista: string | undefined, reservas: Reserva[]): boolean {
-    // Verifica si hay alguna reserva con la misma fecha y hora
-    return !reservas.some(reserva => reserva.fecha === fecha && reserva.hora === hora && reserva.pista === pista);
   }
 
   getReservasExistente() {
