565a425c178c1d1ae4813ede90c61bcea6bb67eb
Saul Matías Jiménez <saulmaji@gmail.com>
2024-05-19 10:17:37 +0200

    Soy tonto

--- padelManager-frontend/src/environment.prod.ts
+++ padelManager-frontend/src/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    BACKEND_URL: 'http://prodiasv19.fis.usal.es', // Aquí puedes definir la URL de producción
+    BACKEND_URL: 'http://prodiasv19.fis.usal.es:8080/api/v1', // Aquí puedes definir la URL de producción
   };
   
